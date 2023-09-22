7e94e9fddcd00fb14fc20878fd0159bf909a749a
parzival-space <me@parzival.space>
2023-09-22 10:36:43 +0200

    created spring boot base

---
+++ src/main/java/de/mmbbs/CrossSolve/CrossSolveApplication.java
@@ -0,0 +1,13 @@
+package de.mmbbs.CrossSolve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossSolveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrossSolveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/mmbbs/CrossSolve/CrossSolveApplicationTests.java
@@ -0,0 +1,13 @@
+package de.mmbbs.CrossSolve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrossSolveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
