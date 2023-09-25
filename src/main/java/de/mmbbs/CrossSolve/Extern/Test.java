97ad25fe5914618a144f6312ab179e19b5a9faee
ASK <ahmaddelo7@gmail.com>
2023-09-25 23:20:09 +0200

    create methode getSoluation

--- src/main/java/de/mmbbs/CrossSolve/Extern/Test.java
+++ src/main/java/de/mmbbs/CrossSolve/Extern/Test.java
@@ -1,24 +1,29 @@
 package de.mmbbs.CrossSolve.Extern;
 
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
 @AllArgsConstructor
 public class Test implements InitializingBean {
 
-    
+    @Autowired
     private CWDB cwdb;
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        log.info(cwdb.url);
+        log.info(cwdb.URL);
+        List result = cwdb.getSoluation("gesetzlich", 5);
+        for (Object object : result) {
+            System.out.println(object);
+        }
     }
 
     
