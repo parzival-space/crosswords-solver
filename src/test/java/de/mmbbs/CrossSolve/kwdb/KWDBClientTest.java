ed8c6b4deb8fb7c030f30c8c908f8604fb68fb74
Parzival <me@parzival.space>
2024-01-25 11:58:43 +0100

    Improved KWDB Client Unit Test

--- src/test/java/de/mmbbs/CrossSolve/kwdb/KWDBClientTest.java
+++ src/test/java/de/mmbbs/CrossSolve/kwdb/KWDBClientTest.java
@@ -3,19 +3,25 @@ package de.mmbbs.CrossSolve.kwdb;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import de.mmbbs.CrossSolve.kwdb.meta.QuestionSolution;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+@Slf4j
 class KWDBClientTest {
 
     @Test
     public void TestKWDB() {
         KWDBClient client = new KWDBClient();
 
-        // ask kwdb
-        List<QuestionSolution> solutions = client.getSolutions("Früchte zum Auflesen", 0);
+        // Question
+        final String query = "Früchte zum Auflesen";
 
+        // ask kwdb
+        List<QuestionSolution> solutions = client.getSolutions(query, 0);
+
+        log.info("Found {} solution(s) for the query '{}'", solutions.size(), query);
         assertFalse(solutions.isEmpty());
     }
 }
