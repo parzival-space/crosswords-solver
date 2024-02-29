071bfedf1f72be0c8c537eaa1e8c413da9f1b693
Parzival <me@parzival.space>
2024-02-29 10:59:05 +0100

    new project structure
    I moved the old one to development-old. Let's be honest, what we did there sucked.

---
+++ src/main/java/de/mmbbs/cwmeteor/CwMeteorApplication.java
@@ -0,0 +1,13 @@
+package de.mmbbs.cwmeteor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CwMeteorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CwMeteorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/mmbbs/cwmeteor/CwmeteorApplicationTests.java
@@ -0,0 +1,13 @@
+package de.mmbbs.cwmeteor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CwmeteorApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
