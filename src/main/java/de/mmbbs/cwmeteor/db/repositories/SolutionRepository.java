3045bc50a7f7e225d051da09bf24d59931522767
Parzival <me@parzival.space>
2024-03-01 13:20:41 +0100

    Feature/Refined API (#14)
    * added database clean task and properties
    
    * added character count support to api

--- src/main/java/de/mmbbs/cwmeteor/db/repositories/SolutionRepository.java
+++ src/main/java/de/mmbbs/cwmeteor/db/repositories/SolutionRepository.java
@@ -10,6 +10,10 @@ import java.util.UUID;
 
 public interface SolutionRepository extends CrudRepository<Solution, UUID> {
     // TODO: make this less exclusive
-    @Query("SELECT s FROM #{#entityName} s WHERE s.question = :question")
+    @Query("SELECT s FROM #{#entityName} s WHERE s.question LIKE %:question%")
     List<Solution> findByQuestion(@Param("question") String question);
+
+
+    @Query("SELECT s FROM #{#entityName} s WHERE s.question LIKE %:question% AND LENGTH(s.answer) = :charCount")
+    List<Solution> findByQuestionAndCharacterCount(@Param("question") String question, @Param("charCount") int charCount);
 }
